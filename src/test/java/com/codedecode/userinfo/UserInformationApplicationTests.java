2958d69dce30783ea09ce3cf1be85fe03138f8ad
santoshngowder <santoshngowder@gmail.com>
2024-05-24 11:53:46 +0530

    first commit

---
+++ src/test/java/com/codedecode/userinfo/UserInformationApplicationTests.java
@@ -0,0 +1,10 @@
+package com.codedecode.userinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInformationApplicationTests {
+
+
+}
